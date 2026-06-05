d7b8327848d7882ca52a7a1aae093edfb2c3b04c
CinthyLu <cramonm1@est.ups.edu.ec>
2026-06-04 19:54:18 -0500

    Strapi

---
+++ src/api/programer/controllers/programer.ts
@@ -0,0 +1,7 @@
+/**
+ * programer controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::programer.programer');
---
+++ src/api/programer/routes/programer.ts
@@ -0,0 +1,7 @@
+/**
+ * programer router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::programer.programer');
